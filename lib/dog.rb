af824ace3df0f48211abc4c1ee65d2770ab52fd1
Martinique <me@martidolce.com>
2020-11-17 11:21:25 -0500

    commit tree b8ac77a146020d428caa43ab78f0c9e5db6544af

---
+++ lib/dog.rb
@@ -0,0 +1,34 @@
+# Author: Martinique Dolce
+# Course: Flatiron School 2020, November 9 - 20201, April 2021
+# Contact: me@martidolce.com | https://modis.martidolce.com
+#
+# Objectives
+# Define new Ruby classes with the class keyword.
+# Instantiate instances of a class.
+# Overview
+# This lab is all about defining classes and instantiating instances.
+#
+# 1. Define Dog in lib/dog.rb
+# Open lib/dog.rb and add a class definition for a Dog class.
+#
+# 2. Make 3 dogs in lib/dog.rb
+# Under your Dog class definition, create three dogs in local variables, fido, snoopy, and lassie.
+#
+# 3. Define a Person in lib/person.rb
+# Open lib/person.rb and add a class definition for a Person class.
+#
+# 4. Make 2 people in lib/person.rb
+# Under your Person class definition, create two people in local variables, adele_goldberg and alan_kay
+
+
+# Get Started:  (1). Open lib/dog.rb and add a class definition for a Dog class.
+class Dog
+
+end
+
+# (2). Under your Dog class definition, create three dogs in local variables, fido, snoopy, and lassie.
+fido = Dog.new
+snoopy = Dog.new
+lassie = Dog.new
+
+# (3). Open lib/person.rb and add a class definition for a Person class. see person.rb
---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+# Author: Martinique Dolce
+# Course: Flatiron School 2020, November 9 - 20201, April 2021
+# Contact: me@martidolce.com | https://modis.martidolce.com
+#
+# Objectives
+# Define new Ruby classes with the class keyword.
+# Instantiate instances of a class.
+# Overview
+# This lab is all about defining classes and instantiating instances.
+
+# (3). Open lib/person.rb and add a class definition for a Person class. see person.rb
+class Person
+
+end
+
+# (4).  Under your Person class definition, create two people in local variables, adele_goldberg and alan_kay
+adele_goldberg = Person.new
+alan_kay = Person.new
